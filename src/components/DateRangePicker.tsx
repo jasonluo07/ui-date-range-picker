db6cfde093868e2397e8f3a48059f66c246585ad
Jason Luo <yulaie1012@gmail.com>
2023-11-25 20:23:45 +0800

    Generate dynamic date buttons for the current month

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -13,6 +13,11 @@ function DateRangePicker() {
     setCurrentMonth(currentMonth.add(1, 'month'));
   }
 
+  const daysInCurrentMonth = Array.from({ length: currentMonth.daysInMonth() }, (_, i) => i + 1);
+  // 這個月的第一天是禮拜幾，這個值會是 0 ~ 6，但是當等於 0 時，會有個問題，應該要是 7，這樣才順序才會正確
+  const firstDayOfCurrentMonth = currentMonth.startOf('month').day() || 7;
+  const lastDayOfCurrentMonth = currentMonth.endOf('month').day() || 7;
+
   return (
     <div className={styles.dateRangePicker}>
       <div className={styles.header}>
@@ -25,42 +30,26 @@ function DateRangePicker() {
         </button>
       </div>
       <div className={styles.days}>
-        {/* TODO: 動態產生日期按鈕，要注意：一個禮拜的第一天是禮拜一 */}
-        <button className={`${styles.dayButton} ${styles.nonCurrentMonth} ${styles.active}`}>29日</button>
-        <button className={`${styles.dayButton} ${styles.nonCurrentMonth} ${styles.active}`}>30日</button>
-        <button className={`${styles.dayButton} ${styles.nonCurrentMonth} ${styles.active}`}>31日</button>
-        <button className={`${styles.dayButton} ${styles.active}`}>1日</button>
-        <button className={`${styles.dayButton} ${styles.active}`}>2日</button>
-        <button className={styles.dayButton}>3日</button>
-        <button className={styles.dayButton}>4日</button>
-        <button className={styles.dayButton}>5日</button>
-        <button className={styles.dayButton}>6日</button>
-        <button className={styles.dayButton}>7日</button>
-        <button className={styles.dayButton}>8日</button>
-        <button className={styles.dayButton}>9日</button>
-        <button className={styles.dayButton}>10日</button>
-        <button className={styles.dayButton}>11日</button>
-        <button className={styles.dayButton}>12日</button>
-        <button className={styles.dayButton}>13日</button>
-        <button className={`${styles.dayButton}`}>14日</button>
-        <button className={`${styles.dayButton}`}>15日</button>
-        <button className={`${styles.dayButton}`}>16日</button>
-        <button className={styles.dayButton}>17日</button>
-        <button className={styles.dayButton}>18日</button>
-        <button className={styles.dayButton}>19日</button>
-        <button className={styles.dayButton}>20日</button>
-        <button className={styles.dayButton}>21日</button>
-        <button className={styles.dayButton}>22日</button>
-        <button className={styles.dayButton}>23日</button>
-        <button className={styles.dayButton}>24日</button>
-        <button className={styles.dayButton}>25日</button>
-        <button className={styles.dayButton}>26日</button>
-        <button className={`${styles.dayButton} ${styles.today}`}>27日</button>
-        <button className={styles.dayButton}>28日</button>
-        <button className={styles.dayButton}>29日</button>
-        <button className={styles.dayButton}>30日</button>
-        <button className={`${styles.dayButton} ${styles.nonCurrentMonth}`}>1日</button>
-        <button className={`${styles.dayButton} ${styles.nonCurrentMonth}`}>2日</button>
+        {/* 上個月要顯示的日期 */}
+        {Array.from({ length: firstDayOfCurrentMonth - 1 }, (_, i) => (
+          <button key={i} className={`${styles.dayButton} ${styles.nonCurrentMonth}`}>
+            {dayjs(currentMonth).subtract(1, 'month').endOf('month').date() - (firstDayOfCurrentMonth - 1) + i}日
+          </button>
+        ))}
+
+        {/* 這個月的所有日期 */}
+        {daysInCurrentMonth.map((day) => (
+          <button key={day} className={styles.dayButton}>
+            {day}日
+          </button>
+        ))}
+
+        {/* 下個月要顯示的日期 */}
+        {Array.from({ length: 7 - lastDayOfCurrentMonth }, (_, i) => (
+          <button key={i} className={`${styles.dayButton} ${styles.nonCurrentMonth}`}>
+            {i + 1}日
+          </button>
+        ))}
       </div>
     </div>
   );

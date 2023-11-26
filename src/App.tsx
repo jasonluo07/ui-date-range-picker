332725ee1fc77cc0fea56862bee4812bf0bed25e
Jason Luo <yulaie1012@gmail.com>
2023-11-26 19:09:17 +0800

    Refactor date range picker and add state for selected dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import DateRangePicker from '@/components/DateRangePicker';
+import { useState } from 'react';
+import { type Dayjs } from 'dayjs';
 import styles from './App.module.css';
+import DateRangePicker from '@/components/DateRangePicker';
 
 const App = () => {
+  const [dateRange, setDateRange] = useState<[Dayjs | null, Dayjs | null]>([null, null]);
+
+  const handleChange = (startDate: Dayjs | null, endDate: Dayjs | null) => {
+    setDateRange([startDate, endDate]);
+  };
+
   return (
     <div className={styles.app}>
-      <DateRangePicker />
+      <div>
+        {dateRange[0]?.format('YYYY-MM-DD')} ~ {dateRange[1]?.format('YYYY-MM-DD')}
+      </div>
+      <DateRangePicker onChange={handleChange} />
     </div>
   );
 };
